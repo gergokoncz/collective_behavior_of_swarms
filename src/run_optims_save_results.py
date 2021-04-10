3d0f5e6a6a13a6c6feb50a312e9488339ce03fab
gergokoncz <gergokoncz06@gmail.com>
2021-04-10 23:43:25 +0200

    couple of runs in

--- src/run_optims_save_results.py
+++ src/run_optims_save_results.py
@@ -11,7 +11,7 @@ from geneal.genetic_algorithms import ContinuousGenAlgSolver
 from simulation.BaseSimulation import *
 from simulation.PSimulation import *
 
-
+genealg_counter = 0
 
 class Logline:
     def __init__(self):
@@ -58,15 +58,26 @@ class Logline:
 logger = Logline()
 
 # set this one
-logger.run = "6"
+logger.run = "1000"
 
 
 def genal_fitness(p):
 
+    global genealg_counter
+    genealg_counter += 1
+    print(genealg_counter)
+    if genealg_counter >= 5:
+        return None
+
     return lambda chromosome: genal_call_psim(chromosome, p)
 
 
-def genal_call_psim(x, p, steps = 500):
+def genal_call_psim(x, p, steps = 5_000):
+    
+    global logger
+    global genealg_counter
+    genealg_counter += 1
+    print(f"sim: {genealg_counter}")
 
     my_sim = ProbabilisticSimulation(
         field_size = (100, 100), 
@@ -81,15 +92,45 @@ def genal_call_psim(x, p, steps = 500):
     for _ in range(steps):
         my_sim.simulate_step()
 
-    #print(my_sim.stored_food)
+    logger.p_leave = str(x[0])
+    logger.p_follow = str(x[1])
+
+    logger.cost = str(my_sim.stored_food)
+
+    logger.write_log_line()
+    print(str(x[0]), str(x[1]))
+    print(my_sim.stored_food)
     return my_sim.stored_food
 
 
 def genal_optim(p):
+    global logger
+
+    logger.optim_alg = 'gen'
+    logger.run = str(int(logger.run) + 1)
+    
+    logger.pso_steps = ""
+    logger.pso_n_particles = ""
+    logger.pso_iters = ""
+
+    logger.gen_pop_size = "25"
+    logger.gen_mutation_rate = "0.1"
+    logger.gen_selection_rate = "0.6"
+    logger.gen_selection_strategy = "roulette_wheel"
+
+    logger.field_x = "100"
+    logger.field_y = "100"
+    logger.n_bots = "10"
+    logger.trail_decay = "100"
+
+    logger.p_resource = str(p)
+    logger.resource_dist_mean = "10"
+    logger.resource_dist_std = "3"
+    
     solver = ContinuousGenAlgSolver(
         n_genes = 2,
         fitness_function = genal_fitness(p),
-        pop_size = 10,
+        pop_size = 25,
         mutation_rate = 0.1,
         selection_rate = 0.6,
         selection_strategy = "roulette_wheel",
@@ -100,9 +141,6 @@ def genal_optim(p):
         plot_results = True
     )
     solver.solve()
-    print("solved")
-    print(solver)
-    print(type(solver))
 
 
 def call_psim(x, p, steps = 5_000):
@@ -155,7 +193,7 @@ def pso_optim(p, steps = 5_000) -> None:
     logger.run = str(int(logger.run) + 1)
     
     logger.pso_steps = str(steps)
-    logger.pso_n_particles = "10"
+    logger.pso_n_particles = "14"
     logger.pso_iters = "20"
 
     logger.gen_pop_size = ""
@@ -172,9 +210,9 @@ def pso_optim(p, steps = 5_000) -> None:
     logger.resource_dist_mean = "10"
     logger.resource_dist_std = "3"
     
-    options = {'c1': 0.5, 'c2': 0.5, 'w': 0.05}
+    options = {'c1': 0.5, 'c2': 0.5, 'w': 0.1}
     bounds = (np.zeros(2), np.ones(2))
-    optimizer = ps.single.GlobalBestPSO(n_particles = 10, dimensions = 2, options = options, bounds = bounds)
+    optimizer = ps.single.GlobalBestPSO(n_particles = 14, dimensions = 2, options = options, bounds = bounds)
     s = optimizer.optimize(call_psim, iters = 20, p = p, steps = steps)
     
     return s
@@ -184,10 +222,10 @@ def pso_optim(p, steps = 5_000) -> None:
 def main() -> None:
     global logger
 
-    for i in [0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008, 0.009]:
-        s = pso_optim(p = i, steps = 5_000)
+    #for i in [0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008, 0.009, 0.01, 0.015, 0.02, 0.025, 0.03, 0.035, 0.04, 0.045, 0.05, 0.055, 0.06, 0.065, 0.07]:
+    #    s = pso_optim(p = i, steps = 5_000)
 
-    #genal_optim(0.01)
+    genal_optim(0.025)
     logger.close_io()
 
 if __name__ == '__main__':
